459044a281d089813af77431b30551b713d86e9c
Sangam <sgsangam@gmail.com>
2013-08-01 19:47:01 -0700

    Hw3 first checkin

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,13 +4,47 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+    self.restore_session
+    # flash[:notice] = "session: #{session}"
+  end
+
+  def save_session(params=nil)
+    session[:sort_type] = @sort_type
+    session[:subnit_type] = @submit_type
+    session[:param_str] = @param_str
+    session[:params] = params
+    if @checkked_buttons != []
+        session[:checkked_button] = @checkked_buttons         
+        session[:all_ratings] = @all_ratings
+    end    
+    if @submit_type == "commit"
+      @submit_str="?utf8="+params["utf8"]
+      params["ratings"].each do |key, val|
+        str = '&ratings['+key+']='+val
+        @submit_str = @submit_str+str
+      end
+      @submit_str = @submit_str+"&commit="+params["commit"]
+    end
+    session[:submit_str] = @submit_str
+  end
+
+  def restore_session
+    @sort_type = session[:sort_type]
+    @param_str = session[:param_str]
+    @submit_type = session[:subnit_type]     
+    @checkked_buttons = session[:checkked_button]        
+    @all_ratings = session[:all_ratings]
+    @submit_str = session[:submit_str]
+    params = session[:params]
   end
 
   def index
     @sort_type = params["sort"]
+    @submit_type = ''
     @all_ratings = Hash.new()
     @checkked_buttons = Array.new
     @param_str = ''
+    @submit_str = ''
     # flash[:notice] = "Params: #{params}"
     all_ratings = Movie.select(:rating).map(&:rating).uniq.sort
     all_ratings.each {|rating| @all_ratings[rating]=false}
@@ -23,6 +57,8 @@ class MoviesController < ApplicationController
         @param_str = @param_str+'&'+rating+'='+val
       end
       @movies = Movie.where(:rating => @checkked_buttons).all
+      @submit_type = "commit"
+      self.save_session(params)      
     elsif @sort_type == 'title' || @sort_type == 'release_date'      
       @all_ratings.each do |rating, rval|
         params.each do |key, val|                 
@@ -39,16 +75,19 @@ class MoviesController < ApplicationController
         # @movies = Movie.order('title')
         @movies = Movie.where(:rating => @checkked_buttons).all
         @movies.sort! {|t1, t2| t1.title <=> t2.title }
+        self.save_session 
       else @sort_type == 'release_date'
         # @movies = Movie.order('release_date')
         @movies = Movie.where(:rating => @checkked_buttons).all
-        @movies.sort! {|r1, r2| r1.release_date <=> r2.release_date}     
+        @movies.sort! {|r1, r2| r1.release_date <=> r2.release_date}
+        self.save_session      
       end
     else
       @movies = Movie.all
       all_ratings.each do |rating|
         @all_ratings[rating]=true
         @param_str = @param_str+'&'+rating+'=1'
+        self.session.clear
       end
     end
   end
@@ -59,7 +98,7 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
+    flash[:notice] = "#{@movie.title} was successfully created."   
     redirect_to movies_path
   end
 
